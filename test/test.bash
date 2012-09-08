871befaf01a47c39f110054e34b9ad9076191f72
Chris Oei <zlkm208-scryptgenpass@yahoo.com>
2012-09-08 16:58:33 -0700

    Updated tests

--- test/test.bash
+++ test/test.bash
@@ -4,12 +4,12 @@ PROG=./scrypt-genpass
 RESULTS=test/test_results.log
 
 $PROG -t > $RESULTS 2>&1
-$PROG -h >> $RESULTS 2>&1
+($PROG -h |grep -v Version)>> $RESULTS 2>&1
 $PROG -p b a >> $RESULTS 2>&1
 $PROG -k test/keyfile1.dat -p abc ghi >> $RESULTS 2>&1
 $PROG -l 2 -p a a >> $RESULTS 2>&1
 $PROG -l 65 -p a a >> $RESULTS 2>&1
 $PROG -l 64 -p a a >> $RESULTS 2>&1
-$PROG -l 4 -p "Speak, friend, and enter." "The Doors of Durin" >> $RESULTS 2>&1
+$PROG -n -l 4 -p "Speak, friend, and enter." "The Doors of Durin" >> $RESULTS 2>&1
 
 diff $RESULTS test/test_results.reference
